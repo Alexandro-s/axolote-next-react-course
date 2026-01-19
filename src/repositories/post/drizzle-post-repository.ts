14b97b3b7faa56d172521de05fdabcd4660f7214
Alexandro <alexandrosantosilva66@gmail.com>
2026-01-19 18:40:54 -0300

    feat: Ajuste no repositore de posts com drizzle

--- src/repositories/post/drizzle-post-repository.ts
+++ src/repositories/post/drizzle-post-repository.ts
@@ -1,6 +1,5 @@
 import { PostModel } from "@/model/post/post-model";
 import { PostRepository } from "./post-repository";
-import { postsTable } from "@/db/drizzle/schemas";
 import { drizzleDb } from "@/db/drizzle";
 import {desc, eq} from "drizzle-orm"
 
@@ -19,22 +18,43 @@ export class DrizzlePostRepository implements PostRepository {
 
 
  async findAll(): Promise<PostModel[]> {
-
+    const posts = await drizzleDb.query.posts.findMany({
+        orderBy: (post, {desc}) => desc(post.createdAt)
+    })
+    return posts;
  }
 async findById(id: string): Promise<PostModel>{
+    console.log('\n', 'findBySlug', '\n')
+    const postId = await drizzleDb.query.posts.findFirst({
+        where: (posts, {eq} ) => eq(posts.id, id)
+    })
+    if(!postId) throw new Error ('Post nao achado')
+        return postId;
 
 }
 async findBySlug(slug: string): Promise<PostModel>{
-
+    const post = await drizzleDb.query.posts.findFirst({
+        where:(posts, {eq, and}) =>
+            and(eq(posts.published, true), eq(posts.slug, slug)),
+    });
+    if(!post) throw new Error('Post n chadado rerefente a slug')
+        return post;
 }
 
 }
 
-(async () => {
-    const repo = new DrizzlePostRepository();
-    const posts = await repo.findAllPublic();
+// nebulosas-planetarias-ultimo-suspiro true
+// pulsars-os-farois-do-universo false
 
-    posts.forEach(post => console.log(post.slug, post.published ))
-})();
+// 3993fcf7-2490-48ed-be2e-58c2030ee764 true
+// be3f14a1-0105-4e2e-bfc9-133a05e7bda6 false
+//'pulsars-os-farois-do-universo
+
+// (async () => {
+//     const repo = new DrizzlePostRepository();
+//     const posts = await repo.findBySlug("nebulosas-planetarias-ultimo-suspiro");
+
+//     console.log(posts)
+// })();
 
 
